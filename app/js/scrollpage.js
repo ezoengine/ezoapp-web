5d76290aeae907b48d6477f01baf2c204434bf1d
oxxo <oxxo.studio@gmail.com>
2014-03-06 16:07:31 +0800

    新增tutorial內容以及左側選單修改

--- app/js/scrollpage.js
+++ app/js/scrollpage.js
@@ -5,21 +5,20 @@ $(function () {
     leftMenuWidth = $('.left-menu').width();
   });
 
-
   $(window).scroll(function (event) {
 
     if ($(window).width() >= 1012) {
-      $(window).scrollTop() > 200 ? $('.left-menu ul').css({
+      $(window).scrollTop() > 200 ? $('.left-menu>ul').css({
         'position': 'fixed',
         'top': '65px',
         'width': leftMenuWidth + 'px'
-      }) : $('.left-menu ul').css({
+      }) : $('.left-menu>ul').css({
         'position': 'relative',
         'top': '0',
         'width': '100%'
       });
     } else {
-      $('.left-menu ul').css({
+      $('.left-menu>ul').css({
         'position': 'relative',
         'top': '0',
         'width': '100%'
@@ -27,30 +26,66 @@ $(function () {
     }
 
     if ($(window).scrollTop() >= 0 && $(window).scrollTop() < ($('#right-2').offset().top - 70)) {
-      $('.left-menu ul li').removeClass('actived');
-      $('.left-menu ul li').eq(0).addClass('actived');
+      $('.left-menu>ul>li').removeClass('actived');
+      $('.left-menu ul li#left-menu-1').addClass('actived');
     }
     if ($(window).scrollTop() >= ($('#right-2').offset().top - 70) && $(window).scrollTop() < ($('#right-3').offset().top - 70)) {
-      $('.left-menu ul li').removeClass('actived');
-      $('.left-menu ul li').eq(1).addClass('actived');
+      $('.left-menu>ul>li').removeClass('actived');
+      $('.left-menu ul li#left-menu-2').addClass('actived');
+      $('.left-menu ul li ul li').removeClass('actived');
     }
     if ($(window).scrollTop() >= ($('#right-3').offset().top - 70) && $(window).scrollTop() < ($('#right-4').offset().top - 70)) {
-      $('.left-menu ul li').removeClass('actived');
-      $('.left-menu ul li').eq(2).addClass('actived');
+      $('.left-menu>ul>li').removeClass('actived');
+      $('.left-menu ul li#left-menu-3').addClass('actived');
     }
     if ($(window).scrollTop() >= ($('#right-4').offset().top - 70)) {
-      $('.left-menu ul li').removeClass('actived');
-      $('.left-menu ul li').eq(3).addClass('actived');
+      $('.left-menu>ul>li').removeClass('actived');
+      $('.left-menu ul li#left-menu-4').addClass('actived');
+    }
+
+
+
+    if ($(window).scrollTop() >= 0 && $(window).scrollTop() < ($('#right-1-2').offset().top - 70)) {
+      $('.left-menu ul li ul li').removeClass('actived');
+      $('.left-menu ul li ul li').eq(0).addClass('actived');
+    }
+    if ($(window).scrollTop() >= ($('#right-1-2').offset().top - 70) && $(window).scrollTop() < ($('#right-1-3').offset().top - 70)) {
+      $('.left-menu ul li ul li').removeClass('actived');
+      $('.left-menu ul li ul li').eq(1).addClass('actived');
+    }
+    if ($(window).scrollTop() >= ($('#right-1-3').offset().top - 70) && $(window).scrollTop() < ($('#right-1-4').offset().top - 70)) {
+      $('.left-menu ul li ul li').removeClass('actived');
+      $('.left-menu ul li ul li').eq(2).addClass('actived');
+    }
+    if ($(window).scrollTop() >= ($('#right-1-4').offset().top - 70) && $(window).scrollTop() < ($('#right-1-5').offset().top - 70)) {
+      $('.left-menu ul li ul li').removeClass('actived');
+      $('.left-menu ul li ul li').eq(3).addClass('actived');
+    }
+    if ($(window).scrollTop() >= ($('#right-1-5').offset().top - 70) && $(window).scrollTop() < ($('#right-1-6').offset().top - 70)) {
+      $('.left-menu ul li ul li').removeClass('actived');
+      $('.left-menu ul li ul li').eq(4).addClass('actived');
+    }
+    if ($(window).scrollTop() >= ($('#right-1-6').offset().top - 70) && $(window).scrollTop() < ($('#right-2').offset().top - 70)) {
+      $('.left-menu ul li ul li').removeClass('actived');
+      $('.left-menu ul li ul li').eq(5).addClass('actived');
     }
 
   });
 
-  $('.left-menu ul li').on('click', function () {
-    var menuIndex = $(this).index() + 1;
+  $('.left-menu>ul>li>a').on('click', function () {
+    var menuIndex = $(this).parent('li').index() + 1;
     menuIndex > 1 ? $("html,body").not(':animated').animate({
       "scrollTop": ($('#right-' + menuIndex).offset().top - 68) + 'px'
     }, 900) : $("html,body").not(':animated').animate({
       "scrollTop": '0'
+    }, 900);
+  });
+
+  $('.left-menu ul li ul li a').on('click', function () {
+    var menuIndex1 = $(this).parent('li').index() + 1;
+    console.log(menuIndex1)
+    $("html,body").not(':animated').animate({
+      "scrollTop": ($('#right-1-' + menuIndex1).offset().top - 68) + 'px'
     }, 900);
   });
 
