f079bd328a67fdc1791969235a5f168b5aa639c2
oxxo <oxxo.studio@gmail.com>
2014-05-13 11:34:31 +0800

    add cn site, and edit layout javascript

--- app/js/layout.js
+++ app/js/layout.js
@@ -1,21 +1,73 @@
-// var path;
 $(function() {
-    'use strict';
-    var $window = $(window);
-    // if (!path) {
-    //   path = ''
-    // };
+  'use strict';
+  var $window = $(window);
 
-    $('head').append(
-        '<link rel="icon" href="favicon.ico" type="image/x-icon"/>' +
-        '<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>'
-    );
+  /*  語系判斷與切換按鈕  */
+  var nowUrl = location.href;
+  var urlParts = nowUrl.split('/');
+  var urlPartsTemp = nowUrl.split('/');
+  var fileName = urlParts.pop();
+  if (fileName == '') {
+    fileName = 'index.html'
+  }
 
-    /**
-     *  google analytics
-     */
-    $('body').append(
-        "<script>\
+  var languageCheck = urlParts.pop();
+  var webUrl = urlPartsTemp.shift() + '//' + urlPartsTemp.shift() + urlPartsTemp.shift() + '/';
+  var enUrl = webUrl + 'en/' + fileName;
+  var cnUrl = webUrl + 'zh-cn/' + fileName;
+  var twUrl = webUrl + fileName;
+
+  if (languageCheck == 'en') {
+    fn_en();
+  } else if (languageCheck == 'zh-cn') {
+    fn_zh_cn();
+  } else {
+    fn_zh_tw();
+  }
+
+  if ($('.language-tw')) {
+    $('.language-tw a').attr('href', twUrl);
+  }
+  if ($('.language-en')) {
+    $('.language-en a').attr('href', enUrl);
+  }
+  if ($('.language-cn')) {
+    $('.language-cn a').attr('href', cnUrl);
+  }
+
+  /* 判斷頁面讓主選單變色 */
+  $('.top-menu li[page=' + $('body').attr('page') + ']').addClass('active').find('a').removeAttr('href');
+
+  /* 版權宣告 */
+  $('.copyright').append(
+    '<div>© InfoChamp Systems Corporation 2014</div>'
+  );
+
+  /* 頁面高度不夠時，footer仍然維持在視窗底部 */
+  if ($('.footer').offset().top < ($window.height() - 100)) {
+    $('.footer').css({
+      'margin-top': ($window.height() - 100 - $('.footer').offset().top) + 'px'
+    });
+  }
+
+  /* 捲動時上方選單高度與背景色變化 */
+  $window.scroll(function() {
+    $window.scrollTop() > 0 ? $('.navbar-inverse').not(':animated').animate({
+      'padding': '0'
+    }).css({
+      'background': 'rgba(5,10,15,.85)'
+    }) : $('.navbar-inverse').animate({
+      'padding': '10px 0'
+    }, {
+      queue: false
+    }).css({
+      'background': 'rgba(7,7,7,1)'
+    });
+  });
+
+  /* google analytics */
+  $('body').append(
+    "<script>\
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\
@@ -23,101 +75,101 @@ $(function() {
     ga('create', 'UA-28403683-1', 'ezoui.com');\
     ga('send', 'pageview');\
     </script>"
-    );
-    /**
-     * 頁面高度不夠時，footer仍然維持在視窗底部
-     */
-    if ($('.footer').offset().top < ($window.height() - 100)) {
-        $('.footer').css({
-            'margin-top': ($window.height() - 100 - $('.footer').offset().top) + 'px'
-        });
-    }
+  );
 
-    /**
-     * 上方主選單
-     */
+  /* 繁體中文 */
+
+  function fn_zh_tw() {
+    $('head').append(
+      '<link rel="icon" href="favicon.ico" type="image/x-icon"/>' +
+      '<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>'
+    );
+
+    /* 上方主選單 */
     $('.top-menu').append(
-        '<li page="index"><a href="index.html">首頁</a></li>' +
-        '<li page="play"><a href="http://jqmdesigner.appspot.com" target="_blank">立刻體驗</a></li>' +
-        '<li page="started"><a href="started.html">基本介紹</a></li>' +
-        '<li page="tutorial"><a href="tutorial_1_basic_grid.html">教學實作</a></li>' +
-        //'<li page="pricing"><a href="'+path+'pricing.html">授權方案</a></li>' +
-        '<li page="aboutus"><a href="aboutus.html">關於我們</a></li>' +
-        '<li page="blog"><a href="http://ezoui.wordpress.com/" target="_blank">Blog</a></li>' +
-        '<li class="language"><a>語系切換</a>' +
-        '<ul>' +
-        '<li class="language-cn"><a>简体中文</a></li>' +
-        '<li class="language-en"><a>English</a></li>' +
-        '</ul>' +
-        '<li class="language-cn"><a>简体中文</a>' +
-        '<li class="language-en"><a>English</a>' +
-        '</li>'
+      '<li page="index"><a href="index.html">首頁</a></li>' +
+      '<li page="play"><a href="http://jqmdesigner.appspot.com" target="_blank">立刻體驗</a></li>' +
+      '<li page="started"><a href="started.html">基本介紹</a></li>' +
+      '<li page="tutorial"><a href="tutorial_1_basic_grid.html">教學實作</a></li>' +
+      '<li page="aboutus"><a href="aboutus.html">關於我們</a></li>' +
+      '<li page="blog"><a href="http://ezoui.wordpress.com/" target="_blank">Blog</a></li>' +
+      '<li class="language"><a>語系切換</a>' +
+      '<ul>' +
+      '<li class="language-cn"><a>简体中文</a></li>' +
+      '<li class="language-en"><a>English</a></li>' +
+      '</ul>' +
+      '</li>' +
+      '<li class="language-cn"><a>简体中文</a></li>' +
+      '<li class="language-en"><a>English</a></li>'
     );
 
     $('.navbar-header').append(
-        '<a class="navbar-brand" href="http://www.ezoui.com" title="EZoApp - 快速開發行動 App 的最佳工具"><img src="img/logo.png" style="width:27px; margin:-3px 5px 0 0; border:none;">EZoApp</a>'
+      '<a class="navbar-brand" href="http://www.ezoui.com" title="EZoApp - 快速開發行動 App 的最佳工具"><img src="img/logo.png" style="width:27px; margin:-3px 5px 0 0; border:none;">EZoApp</a>'
     );
 
-    /**
-     * 判斷頁面讓主選單變色
-     */
-    $('.top-menu li[page=' + $('body').attr('page') + ']').addClass('active').find('a').removeAttr('href');
-
-    /**
-     * footer選單
-     */
+    /* footer選單 */
     $('.footer-menu').append(
-        '<li page="index"><a href="index.html">首頁</a></li>' +
-        '<li page="plays"><a href="http://jqmdesigner.appspot.com" target="_blank">立刻體驗</a></li>' +
-        '<li page="started"><a href="started.html">基本介紹</a></li>' +
-        '<li page="tutorial"><a href="tutorial_1_basic_grid.html">教學實作</a></li>' +
-        '<li page="aboutus"><a href="aboutus.html">關於我們</a></li>' +
-        '<li page="blog"><a href="http://ezoui.wordpress.com/" target="_blank">Blog</a></li>'
+      '<li page="index"><a href="index.html">首頁</a></li>' +
+      '<li page="plays"><a href="http://jqmdesigner.appspot.com" target="_blank">立刻體驗</a></li>' +
+      '<li page="started"><a href="started.html">基本介紹</a></li>' +
+      '<li page="tutorial"><a href="tutorial_1_basic_grid.html">教學實作</a></li>' +
+      '<li page="aboutus"><a href="aboutus.html">關於我們</a></li>' +
+      '<li page="blog"><a href="http://ezoui.wordpress.com/" target="_blank">Blog</a></li>'
     );
     $('.social-menu').append(
-        '<li><a href="mailto:EZoService@icsc.com.tw" target="_blank"><img src="img/logo05.png" alt="mail to us" title="mail to us" /></a></li>' +
-        '<li><a href="https://plus.google.com/117990746163074166131" target="_blank"><img src="img/logo01.png" alt="google+" title="Find us on Google+" /></a></li>' +
-        '<li><a href="https://www.facebook.com/EZoUI" target="_blank"><img src="img/logo02.png" alt="faacebook" title="Find us on Facebook" /></a></li>' +
-        '<li><a href="https://twitter.com/EZoService/status/405605560386461696" target="_blank"><img src="img/logo03.png" alt="twitter" title="Find us on Twitter" /></a></li>' +
-        '<li><a href="http://ezoui.wordpress.com/" target="_blank"><img src="img/logo04.png" alt="blog" title="Find us on blog" /></a></li>'
+      '<li><a href="mailto:EZoService@icsc.com.tw" target="_blank"><img src="img/logo05.png" alt="mail to us" title="mail to us" /></a></li>' +
+      '<li><a href="https://plus.google.com/117990746163074166131" target="_blank"><img src="img/logo01.png" alt="google+" title="Find us on Google+" /></a></li>' +
+      '<li><a href="https://www.facebook.com/EZoUI" target="_blank"><img src="img/logo02.png" alt="faacebook" title="Find us on Facebook" /></a></li>' +
+      '<li><a href="https://twitter.com/EZoService/status/405605560386461696" target="_blank"><img src="img/logo03.png" alt="twitter" title="Find us on Twitter" /></a></li>' +
+      '<li><a href="http://ezoui.wordpress.com/" target="_blank"><img src="img/logo04.png" alt="blog" title="Find us on blog" /></a></li>'
     );
-    $('.copyright').append(
-        '<div>© InfoChamp Systems Corporation 2014</div>'
+  }
+
+  /* 简体中文 */
+
+  function fn_zh_cn() {
+    $('head').append(
+      '<link rel="icon" href="../favicon.ico" type="image/x-icon"/>' +
+      '<link rel="shortcut icon" href="../favicon.ico" type="image/x-icon"/>'
     );
 
+    /* 上方主选单 */
+    $('.top-menu').append(
+      '<li page="index"><a href="index.html">首页</a></li>' +
+      '<li page="play"><a href="http://jqmdesigner.appspot.com" target="_blank">立刻体验</a></li>' +
+      '<li page="started"><a href="started.html">基本介绍</a></li>' +
+      '<li page="tutorial"><a href="tutorial_1_basic_grid.html">教学实作</a></li>' +
+      '<li page="aboutus"><a href="aboutus.html">关于我们</a></li>' +
+      '<li page="blog"><a href="http://ezoui.wordpress.com/" target="_blank">Blog</a></li>' +
+      '<li class="language"><a>语系切换</a>' +
+      '<ul>' +
+      '<li class="language-tw"><a>繁体中文</a></li>' +
+      '<li class="language-en"><a>English</a></li>' +
+      '</ul>' +
+      '<li class="language-tw"><a>繁体中文</a>' +
+      '<li class="language-en"><a>English</a>' +
+      '</li>'
+    );
 
-    /**
-     * 捲動時上方選單高度與背景色變化
-     */
-    $window.scroll(function() {
-        $window.scrollTop() > 0 ? $('.navbar-inverse').not(':animated').animate({
-            'padding': '0'
-        }).css({
-            'background': 'rgba(5,10,15,.85)'
-        }) : $('.navbar-inverse').animate({
-            'padding': '10px 0'
-        }, {
-            queue: false
-        }).css({
-            'background': 'rgba(7,7,7,1)'
-        });
-    });
+    $('.navbar-header').append(
+      '<a class="navbar-brand" href="http://www.ezoui.com" title="EZoApp - 快速开发行动 App 的最佳工具"><img src="../img/logo.png" style="width:27px; margin:-3px 5px 0 0; border:none;">EZoApp</a>'
+    );
 
-    /*  語系切換  */
-    var nowUrl = location.href;
-    var urlParts = nowUrl.split("/");
-    var fileName = urlParts.pop();
-    var webUrl = "http://127.0.0.1:9000/"
-    var enUrl = webUrl + "en/" + fileName;
-    var cnUrl = webUrl + "cn/" + fileName;
-    var twUrl = webUrl + fileName;
-    if ($('.language-tw')) {
-        $('.language-tw a').attr('href', twUrl);
-    }
-    if ($('.language-en')) {
-        $('.language-en a').attr('href', enUrl);
-    }
-    if ($('.language-cn')) {
-        $('.language-cn a').attr('href', cnUrl);
-    }
+    /* footer选单 */
+    $('.footer-menu').append(
+      '<li page="index"><a href="index.html">首页</a></li>' +
+      '<li page="plays"><a href="http://jqmdesigner.appspot.com" target="_blank">立刻体验</a></li>' +
+      '<li page="started"><a href="started.html">基本介绍</a></li>' +
+      '<li page="tutorial"><a href="tutorial_1_basic_grid.html">教学实作</a></li>' +
+      '<li page="aboutus"><a href="aboutus.html">关于我们</a></li>' +
+      '<li page="blog"><a href="http://ezoui.wordpress.com/" target="_blank">Blog</a></li>'
+    );
+    $('.social-menu').append(
+      '<li><a href="mailto:EZoService@icsc.com.tw" target="_blank"><img src="../img/logo05.png" alt="mail to us" title="mail to us" /></a></li>' +
+      '<li><a href="https://plus.google.com/117990746163074166131" target="_blank"><img src="../img/logo01.png" alt="google+" title="Find us on Google+" /></a></li>' +
+      '<li><a href="https://www.facebook.com/EZoUI" target="_blank"><img src="../img/logo02.png" alt="faacebook" title="Find us on Facebook" /></a></li>' +
+      '<li><a href="https://twitter.com/EZoService/status/405605560386461696" target="_blank"><img src="../img/logo03.png" alt="twitter" title="Find us on Twitter" /></a></li>' +
+      '<li><a href="http://ezoui.wordpress.com/" target="_blank"><img src="../img/logo04.png" alt="blog" title="Find us on blog" /></a></li>'
+    );
+  }
 });
--- app/js/layout_tutorial.js
+++ app/js/layout_tutorial.js
@@ -3,52 +3,32 @@ $(function() {
   var $window = $(window),
     windowHeight, menuWidth, menuHeight, menuTop, scrollNum, menuHeight;
 
+  /*  語系判斷與切換按鈕  */
+  var nowUrl = location.href;
+  var urlParts = nowUrl.split('/');
+  var fileName = urlParts.pop();
+  var languageCheck = urlParts.pop();
+  console.log(languageCheck)
+
+  if (languageCheck == 'en') {
+    fn_en();
+  } else if (languageCheck == 'zh-cn') {
+    fn_zh_cn();
+  } else {
+    fn_zh_tw();
+  }
+
   $('.tutorial-left-menu>ul').css({
     'height': $window.height() - 90 + 'px'
   });
   menuHeight = $('.tutorial-left-menu>ul').height();
   menuWidth = $('.tutorial-left-menu').width();
 
-  $('.banner-content-text').append(
-    '<h1>EZoApp 教學實作</h1>' +
-    '<h2>學習快速開發 App 的程式饗宴</h2>'
-  );
   $('pre').addClass('prettyprint');
 
-  /**
-   * tutorial 左側選單
-   */
-
-  $('.tutorial-left-menu ul').append(
-    '<li><a>基本元件實作</a>' +
-    '<ul>' +
-    '<li page-content="tutorial_1_basic_grid"><a href="tutorial_1_basic_grid.html">1. grid 基礎排版</a></li>' +
-    '<li page-content="tutorial_2_button"><a href="tutorial_2_button.html">2. 製作特色按鈕</a></li>' +
-    '<li page-content="tutorial_3_google_font"><a href="tutorial_3_google_font.html">3. 使用 Google Fonts</a></li>' +
-    '<li page-content="tutorial_4_droplist"><a href="tutorial_4_droplist.html">4. 建立下拉選單</a></li>' +
-    '<li page-content="tutorial_5_checklist"><a href="tutorial_5_checkbox.html">5. 設計 CheckBox</a></li>' +
-    '<li page-content="tutorial_6_home_page"><a href="tutorial_6_home_page.html">6. 打造入口頁面</a></li>' +
-    '<li page-content="tutorial_7_button"><a href="tutorial_7_button.html">7. 按鈕切換設定<span></span></a></li>' +
-    '</ul>' +
-    '</li>' +
-    '<li><a>JavaScript 應用</a><i></i>' +
-    '<ul>' +
-    '<li page-content="tutorial_js_1_googlemap"><a href="tutorial_js_1_googlemap.html">1. Google 地圖應用</a></li>' +
-    '<li page-content="tutorial_js_2_swipe_page"><a href="tutorial_js_2_swipe_page.html">2. 滑動頁面實作</a></li>' +
-    '<li page-content="tutorial_js_3_star_widget"><a href="tutorial_js_3_star_widget.html">3. 星號分數評比</a></li>' +
-    '<li page-content="tutorial_js_4_open_data"><a href="tutorial_js_4_open_data.html">4. 串接開放資料</a></li>' +
-    '<li page-content="tutorial_js_5_rss"><a href="tutorial_js_5_rss.html">5. RSS 服務實作<span></span></a></li>' +
-    '</ul>' +
-    '</li>'
-  );
-
-  /**
-   * 判斷頁面讓左側選單變色
-   */
+  /* 判斷頁面讓左側選單變色 */
   $('.tutorial-left-menu ul li ul li[page-content=' + $('body').attr('page-content') + ']').addClass('active').find('a').removeAttr('href');
   $('.tutorial-left-menu ul li ul li[page-content=' + $('body').attr('page-content') + ']').parent('ul').parent('li').addClass('active');
-
-
 
   $window.resize(function() {
     windowHeight = $window.height();
@@ -65,9 +45,8 @@ $(function() {
   $window.scroll(function() {
     scrollNum = $(document).height() - menuHeight - 90;
     menuTop = $('.tutorial-left-menu>ul').offset().top;
-    /**
-     * 卷軸往下捲到一定程度，左側選單會固定不動
-     */
+
+    /* 卷軸往下捲到一定程度，左側選單會固定不動 */
     if ($window.width() >= 978) {
       if ($window.scrollTop() > 200 && $window.scrollTop() < scrollNum) {
         $('.tutorial-left-menu>ul').css({
@@ -100,7 +79,68 @@ $(function() {
         'width': '100%'
       });
     }
-
   });
 
+  function fn_zh_tw() {
+    $('.banner-content-text').append(
+      '<h1>EZoApp 教學實作</h1>' +
+      '<h2>學習快速開發 App 的程式饗宴</h2>'
+    );
+
+    /* tutorial 左側選單 */
+    $('.tutorial-left-menu ul').append(
+      '<li><a>基本元件實作</a>' +
+      '<ul>' +
+      '<li page-content="tutorial_1_basic_grid"><a href="tutorial_1_basic_grid.html">1. grid 基礎排版</a></li>' +
+      '<li page-content="tutorial_2_button"><a href="tutorial_2_button.html">2. 製作特色按鈕</a></li>' +
+      '<li page-content="tutorial_3_google_font"><a href="tutorial_3_google_font.html">3. 使用 Google Fonts</a></li>' +
+      '<li page-content="tutorial_4_droplist"><a href="tutorial_4_droplist.html">4. 建立下拉選單</a></li>' +
+      '<li page-content="tutorial_5_checklist"><a href="tutorial_5_checkbox.html">5. 設計 CheckBox</a></li>' +
+      '<li page-content="tutorial_6_home_page"><a href="tutorial_6_home_page.html">6. 打造入口頁面</a></li>' +
+      '<li page-content="tutorial_7_button"><a href="tutorial_7_button.html">7. 按鈕切換設定<span></span></a></li>' +
+      '</ul>' +
+      '</li>' +
+      '<li><a>JavaScript 應用</a><i></i>' +
+      '<ul>' +
+      '<li page-content="tutorial_js_1_googlemap"><a href="tutorial_js_1_googlemap.html">1. Google 地圖應用</a></li>' +
+      '<li page-content="tutorial_js_2_swipe_page"><a href="tutorial_js_2_swipe_page.html">2. 滑動頁面實作</a></li>' +
+      '<li page-content="tutorial_js_3_star_widget"><a href="tutorial_js_3_star_widget.html">3. 星號分數評比</a></li>' +
+      '<li page-content="tutorial_js_4_open_data"><a href="tutorial_js_4_open_data.html">4. 串接開放資料</a></li>' +
+      '<li page-content="tutorial_js_5_rss"><a href="tutorial_js_5_rss.html">5. RSS 服務實作<span></span></a></li>' +
+      '</ul>' +
+      '</li>'
+    );
+  }
+
+  function fn_zh_cn() {
+    $('.banner-content-text').append(
+      '<h1>EZoApp 教学实作</h1>' +
+      '<h2>学习快速开发 App 的程序飨宴</h2>'
+    );
+
+    /* tutorial 左侧选单 */
+    $('.tutorial-left-menu ul').append(
+      '<li><a>基本组件实作</a>' +
+      '<ul>' +
+      '<li page-content="tutorial_1_basic_grid"><a href="tutorial_1_basic_grid.html">1. grid 基础排版</a></li>' +
+      '<li page-content="tutorial_2_button"><a href="tutorial_2_button.html">2. 制作特色按钮</a></li>' +
+      '<li page-content="tutorial_3_google_font"><a href="tutorial_3_google_font.html">3. 使用 Google Fonts</a></li>' +
+      '<li page-content="tutorial_4_droplist"><a href="tutorial_4_droplist.html">4. 建立下拉选单</a></li>' +
+      '<li page-content="tutorial_5_checklist"><a href="tutorial_5_checkbox.html">5. 设计 CheckBox</a></li>' +
+      '<li page-content="tutorial_6_home_page"><a href="tutorial_6_home_page.html">6. 打造入口页面</a></li>' +
+      '<li page-content="tutorial_7_button"><a href="tutorial_7_button.html">7. 按钮切换设定<span></span></a></li>' +
+      '</ul>' +
+      '</li>' +
+      '<li><a>JavaScript 应用</a><i></i>' +
+      '<ul>' +
+      '<li page-content="tutorial_js_1_googlemap"><a href="tutorial_js_1_googlemap.html">1. Google 地图应用</a></li>' +
+      '<li page-content="tutorial_js_2_swipe_page"><a href="tutorial_js_2_swipe_page.html">2. 滑动页面实作</a></li>' +
+      '<li page-content="tutorial_js_3_star_widget"><a href="tutorial_js_3_star_widget.html">3. 星号分数评比</a></li>' +
+      '<li page-content="tutorial_js_4_open_data"><a href="tutorial_js_4_open_data.html">4. 串接开放资料</a></li>' +
+      '<li page-content="tutorial_js_5_rss"><a href="tutorial_js_5_rss.html">5. RSS 服务实作<span></span></a></li>' +
+      '</ul>' +
+      '</li>'
+    );
+
+  }
 });
